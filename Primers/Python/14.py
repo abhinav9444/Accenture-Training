a02e4927181a433cca0a10141b5cea9b58a2bebe
abhinav9444 <abhinavkrsingh9444@gmail.com>
2026-03-03 19:33:14 +0530

    Primer Python Code

---
+++ Primers/Python/14.py
@@ -0,0 +1,12 @@
+import turtle
+# howard = turtle.Turtle()
+# for side in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+#     howard.forward(side * 10)
+#     howard.right(90 - side)
+
+import turtle
+t = turtle.Turtle()
+t.color("red")
+for side in range(100):
+    t.forward(side)
+    t.right(45)
